011d00a797425e971336e197e7f454d8b1d34c15
heber3000 <heberlevy@gmail.com>
2022-11-17 18:47:47 -0300

    criado atributo classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2
+
     def __init__(self, *filhos, nome = None, idade = 32):
         self.idade = idade
         self.nome = nome
@@ -21,8 +23,15 @@ if __name__ == '__main__':
 
     heber.sobrenome = 'Almeida'
     del heber.filhos
+    levy.olhos = 1
     print(heber.__dict__)
     print(levy.__dict__)
+    print(Pessoa.olhos)
+    Pessoa.olhos = 3
+    del levy.olhos
+    print(heber.olhos)
+    print(levy.olhos)
+    print(id(Pessoa.olhos), id(heber.olhos), id(levy.olhos))
 
 
 
