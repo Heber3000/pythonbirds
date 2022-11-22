3196cc2fdabb7d90123af12571302e42f86585aa
heber3000 <heberlevy@gmail.com>
2022-11-22 19:27:30 -0300

    adicionando obstáculos

--- fase.py
+++ fase.py
@@ -27,7 +27,7 @@ class Ponto():
 class Fase():
     def __init__(self, intervalo_de_colisao=1):
         """
-        Método que Inicializa uma fase.
+        Método que inicializa uma fase.
 
         :param intervalo_de_colisao:
         """
